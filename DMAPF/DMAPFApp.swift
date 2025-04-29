16545d8b4400a029c4f41ad32ab40b49030e0f2d
Kazuki Minami <wakarumaji3@gmail.com>
2025-04-29 16:03:08 +0900

    Initial Commit

---
+++ DMAPF/DMAPFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DMAPFApp.swift
+//  DMAPF
+//
+//  Created by Kazuki Minami on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct DMAPFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
